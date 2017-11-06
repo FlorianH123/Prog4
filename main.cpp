2c7fe574e5e9e33fc791b9bd68320d51484498fd
FlorianH123 <f.hennrich@gmx.de>
2017-11-06 15:40:52 +0100

    svssv

--- main.cpp
+++ main.cpp
@@ -19,13 +19,15 @@ void bubbleSort(student *array, int elemente) {
     int i;
     student temp{};
 
-    while(elemente--)
-        for(i = 1; i <= elemente; i++)
-            if(array[i-1].notenSchnitt > array[i].notenSchnitt) {
-                temp=array[i];
-                array[i]=array[i-1];
-                array[i-1]=temp;
+    while((elemente--) > 0) {
+        for (i = 1; i <= elemente; i++) {
+            if (array[i - 1].notenSchnitt > array[i].notenSchnitt) {
+                temp = array[i];
+                array[i] = array[i - 1];
+                array[i - 1] = temp;
             }
+        }
+    }
 }
 
 void readStudentFile() {
